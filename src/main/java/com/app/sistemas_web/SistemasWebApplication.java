acf38991524826f462a651c21d6a00fcf8b31208
mateussperess <mateuspereslopesl@outlook.com>
2026-05-26 21:23:51 -0300

    first commit

---
+++ src/main/java/com/app/sistemas_web/SistemasWebApplication.java
@@ -0,0 +1,13 @@
+package com.app.sistemas_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/sistemas_web/SistemasWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sistemas_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
